1d7f4a145a25f7b591cc7b5e6f585e6a80ffd85d
LucaBarden <lubar105@hhu.de>
2022-03-07 10:53:19 +0100

    Fix v2

--- src/test/java/de/propra/chicken/ChickenApplicationTests.java
+++ src/test/java/de/propra/chicken/ChickenApplicationTests.java
@@ -7,9 +7,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ChickenApplicationTests {
 
+    /*
     @Test
-    @Disabled
     void contextLoads() {
     }
+    */
 
 }
