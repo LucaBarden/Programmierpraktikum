0e4ac717a21de0bf10bfb1b7a979f317912b8c5c
LucaBarden <lubar105@hhu.de>
2022-03-07 09:03:03 +0100

    Init

---
+++ src/test/java/de/propra/chicken/ChickenApplicationTests.java
@@ -0,0 +1,13 @@
+package de.propra.chicken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChickenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
