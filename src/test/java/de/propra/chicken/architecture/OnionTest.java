39cd6c59b26d00ea28c402b6aa8129b3702eca00
Luca Barden <lubar105@hhu.de>
2022-03-08 09:57:43 +0100

    Erneuter Git Fix

--- src/test/java/de/propra/chicken/architecture/OnionTest.java
+++ src/test/java/de/propra/chicken/architecture/OnionTest.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package de.propra.chicken.architecture;
 
 import com.tngtech.archunit.junit.AnalyzeClasses;
@@ -19,26 +18,5 @@ public class OnionTest {
             .adapter("web", "..web..")
             .adapter("db", "..repository..");
 
-=======
-package de.propra.chicken.architecture;
 
-import com.tngtech.archunit.junit.AnalyzeClasses;
-import com.tngtech.archunit.junit.ArchTest;
-import com.tngtech.archunit.lang.ArchRule;
-
-import static com.tngtech.archunit.library.Architectures.onionArchitecture;
-
-@AnalyzeClasses(packages = "de.propra.chicken")
-public class OnionTest {
-
-    @ArchTest
-    ArchRule onionTest = onionArchitecture()
-            .domainModels("..domain.model..")
-            .domainServices("..domain.service..")
-            .applicationServices("..service..")
-
-            .adapter("web", "..web..")
-            .adapter("db", "..repository..");
-
->>>>>>> mob/main
 }
