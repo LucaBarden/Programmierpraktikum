13623c58fbbd15009b81804454d8b7d0c0f1e644
Luca Barden <lubar105@hhu.de>
2022-03-11 13:13:11 +0100

    Deleted Build Files

--- src/test/java/de/propra/chicken/application/KlausurServiceTest.java
+++ src/test/java/de/propra/chicken/application/KlausurServiceTest.java
@@ -11,7 +11,7 @@ import static org.mockito.Mockito.*;
 
 public class KlausurServiceTest {
 
-    
+
     @Test
     @DisplayName("Testet ob eine ungueltige LSF ID einen Fehler wirft")
     public void validLSFID(){
