dacd871bcb26b3f87b0e16827219b3af87e0da2e
Luca Barden <lubar105@hhu.de>
2022-03-11 12:55:03 +0100

    Spotbugs
    Signed-off-by: Luca Barden <lubar105@hhu.de>

--- src/test/java/de/propra/chicken/application/KlausurServiceTest.java
+++ src/test/java/de/propra/chicken/application/KlausurServiceTest.java
@@ -2,7 +2,6 @@ package de.propra.chicken.application;
 
 import de.propra.chicken.application.service.KlausurService;
 import de.propra.chicken.application.service.repository.KlausurRepository;
-import de.propra.chicken.db.KlausurRepositoryImpl;
 import de.propra.chicken.domain.model.Klausur;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,16 +11,16 @@ import static org.mockito.Mockito.*;
 
 public class KlausurServiceTest {
 
-
+    
     @Test
-    @DisplayName("Testet ob eine ugültige LSF ID einen Fehler wirft")
+    @DisplayName("Testet ob eine ungueltige LSF ID einen Fehler wirft")
     public void validLSFID(){
         KlausurRepository repo = mock(KlausurRepository.class);
         KlausurService service = new KlausurService(repo);
         doNothing().when(repo).save(any());
 
         assertThrows(Exception.class, () -> {
-            service.save(new Klausur("Test", 219960, false, null, null, null));
+            service.save(new Klausur("Test", 123, false, null, null, null));
         });
     }
 
