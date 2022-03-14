36a159f17c1b585926ca0796214681fb292165d2
Luca Barden <lubar105@hhu.de>
2022-03-14 11:10:37 +0100

    V1 SQL fixed
    Signed-off-by: Luca Barden <lubar105@hhu.de>

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,39 +1,46 @@
-CREATE TABLE `klausuren` (
-    `name` TEXT NOT NULL,
-    `lsfID` INT(16) unsigned NOT NULL UNIQUE,
-    `datum` DATE NOT NULL,
-    `beginn` TIME NOT NULL,
-    `schluss` TIME NOT NULL,
-    `isPraesenz` BOOLEAN NOT NULL,
-    PRIMARY KEY (`lsfID`)
+CREATE TABLE IF NOT EXISTS
+    klausuren (
+    name varchar(100) not null,
+    lsfID int not null unique,
+    datum date not null,
+    beginn time not null,
+    schluss time not null,
+    isPraesenz boolean not null,
+    primary key (lsfID)
     );
 
-CREATE TABLE `studenten` (
-    `githubID` INT(16) unsigned NOT NULL UNIQUE,
-    `resturlaub` INT(4) NOT NULL,
-    PRIMARY KEY (`githubID`)
-    );
-
-
-CREATE TABLE `KlausurStudent` (
-    `lsfID` INT(16) unsigned NOT NULL,
-    `githubID` INT(16) unsigned NOT NULL,
-    PRIMARY KEY (`lsfID`,`githubID`)
+CREATE TABLE IF NOT EXISTS
+    studenten (
+    githubID INT NOT NULL UNIQUE,
+    resturlaub INT NOT NULL,
+    PRIMARY KEY (githubID)
     );
 
 
-CREATE TABLE `urlaub` (
-                          `datum` DATE NOT NULL,
-                          `beginn` TIME,
-                          `schluss` TIME,
-                          `githubID` INT(16) unsigned,
-    `id` INT unsigned NOT NULL AUTO_INCREMENT
-                            PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS
+    klausurStudent (
+    lsfID INT NOT NULL,
+    githubID INT NOT NULL,
+    PRIMARY KEY (lsfID,githubID)
     );
 
-CREATE TABLE `auditLog` (
-                            `id` INT unsigned NOT NULL AUTO_INCREMENT,
-                            `githubID` INT NOT NULL,
-                            `timestamp` DATETIME NOT NULL,
-                            PRIMARY KEY (`id`)
+
+CREATE TABLE IF NOT EXISTS
+    urlaub (
+    id INT NOT NULL auto_increment,
+    datum DATE NOT NULL,
+    beginn TIME not null,
+    schluss TIME not null,
+    githubID INT not null,
+    constraint urlaub
+    PRIMARY KEY (id)
+    );
+
+CREATE TABLE IF NOT EXISTS
+    auditLog (
+        id INT unsigned NOT NULL AUTO_INCREMENT,
+        githubID INT NOT NULL,
+        timestamp DATETIME NOT NULL,
+        constraint auditLog
+        PRIMARY KEY (id)
     );
