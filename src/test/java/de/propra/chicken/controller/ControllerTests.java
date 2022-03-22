9270812e0911df97aa5e711e81322c000989f3a1
Luca Barden <lubar105@hhu.de>
2022-03-22 10:51:19 +0100

    ControllerTests / Restriktionen in den Formularen entfernt

--- src/test/java/de/propra/chicken/controller/ControllerTests.java
+++ src/test/java/de/propra/chicken/controller/ControllerTests.java
@@ -5,6 +5,7 @@ import de.propra.chicken.application.service.Service;
 import de.propra.chicken.configuration.MethodSecurityConfiguration;
 import de.propra.chicken.domain.model.Klausur;
 import de.propra.chicken.domain.model.Student;
+import de.propra.chicken.domain.model.Urlaub;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -32,8 +33,7 @@ import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -72,6 +72,16 @@ public class ControllerTests {
                 new OAuth2UserAuthority("ROLE_" + role.toUpperCase(), attributes));
         OAuth2User user = new DefaultOAuth2User(authorities, attributes, "login");
         return new OAuth2AuthenticationToken(user, authorities, "whatever");
+    }
+
+        OAuth2User buildOAuth2User(String role, String name) {
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("login", name);
+            attributes.put("id", 12345);
+
+            List<GrantedAuthority> authorities = Collections.singletonList(
+                    new OAuth2UserAuthority("ROLE_" + role.toUpperCase(), attributes));
+            return new DefaultOAuth2User(authorities, attributes, "login");
     }
 
     @Test
@@ -147,6 +157,59 @@ public class ControllerTests {
                         .with(csrf()))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/student"));
+
+}
+
+    @Test
+    @DisplayName("Prueft dass die Service Methoden bei einer Klausuranmeldung richtig aufgerufen werden")
+    void klausuranmeldenService() throws Exception {
+        OAuth2User oAuth2User = buildOAuth2User("user", "Max Mustermann");
+        mockMvc.perform(post("/klausurAnmelden").session(session)
+                        .param("id", "12345")
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/student"));
+
+        verify(service).klausurAnmeldung(12345L, oAuth2User);
+
+
     }
+
+    @Test
+    @DisplayName("Prueft dass die Service Methoden bei einer Klausur Erstellung richtig aufgerufen werden")
+    void klausuranlegenService() throws Exception {
+        Klausur klausur = new Klausur("test", 12345L, true, LocalDate.now().toString(), LocalTime.now().toString(), LocalTime.now().plusHours(1).toString());
+        OAuth2User oAuth2User = buildOAuth2User("user", "Max Mustermann");
+        mockMvc.perform(post("/klausurErstellen").session(session)
+                        .param("name", klausur.getName())
+                        .param("_praesenz", "on")
+                        .param("lsfid", String.valueOf(klausur.getLsfid()))
+                        .param("datum", klausur.getDatum().toString())
+                        .param("beginn", klausur.getBeginn().toString())
+                        .param("ende", klausur.getEnde().toString())
+                        .with(csrf()))
+
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/klausur"));
+
+        verify(service).saveKlausur(klausur, oAuth2User);
+    }
+
+    @Test
+    @DisplayName("Prueft ob die Service methoden fuers erstellen eines Urlaubs richtig aufgerufen werden")
+    void urlaubPostService() throws Exception {
+        OAuth2User oAuth2User = buildOAuth2User("user", "Max Mustermann");
+        Urlaub urlaub = new Urlaub(LocalDate.now().toString(), LocalTime.now().toString(), LocalTime.now().plusHours(1).toString());
+        mockMvc.perform(post("/urlaubErstellen").session(session)
+                        .param("tag", urlaub.getTag().toString())
+                        .param("beginn", urlaub.getBeginn().toString())
+                        .param("end", urlaub.getEnd().toString())
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/student"));
+
+        verify(service).speicherUrlaub(urlaub, 12345, oAuth2User);
+    }
+
 
 }
